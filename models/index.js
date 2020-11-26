9506d260f0302d9132b54550815b021db03cf73a
Juan Kaenel <kaenel10@gmail.com>
2020-11-26 00:30:58 -0300

    Modelo Person

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 import Category from './category';
 import Article from './article';
 import User from './user';
+import Person from './person';
 
 export default {
     Category,
     Article,
-    User
+    User,
+    Person
 }
