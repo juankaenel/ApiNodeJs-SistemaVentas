e6f22aaca642af8062bfafc7df2f7a86b82a7af0
Juan Kaenel <kaenel10@gmail.com>
2020-11-26 00:21:43 -0300

    Aplicando middleware de autenticación a article y category

--- routes/article.js
+++ routes/article.js
@@ -1,14 +1,16 @@
 import routerx from 'express-promise-router';
 import articleController from '../controllers/ArticleController';
+import auth from '../middlewares/auth';
 
 const router = routerx(); // routerx usamos para que las rutas nos devuelvan un promise
 
-router.post('/add', articleController.add);
-router.get('/query', articleController.query);
-router.get('/list', articleController.list);
-router.put('/update', articleController.update);
-router.put('/activate', articleController.activate);
-router.put('/deactivate', articleController.deactivate);
-router.delete('/remove', articleController.remove);
+// solo los administradores y vendedores tienen acceso a estas rutas
+router.post('/add',auth.verifySeller ,articleController.add);
+router.get('/query',auth.verifySeller ,articleController.query);
+router.get('/list',auth.verifySeller ,articleController.list);
+router.put('/update',auth.verifySeller ,articleController.update);
+router.put('/activate',auth.verifySeller ,articleController.activate);
+router.put('/deactivate',auth.verifySeller ,articleController.deactivate);
+router.delete('/remove',auth.verifySeller ,articleController.remove);
 
 export default router;
--- routes/category.js
+++ routes/category.js
@@ -1,14 +1,15 @@
 import routerx from 'express-promise-router';
 import categoryController from '../controllers/CategoryController';
-
+import auth from '../middlewares/auth';
 const router = routerx(); // routerx usamos para que las rutas nos devuelvan un promise
 
-router.post('/add', categoryController.add);
-router.get('/query', categoryController.query);
-router.get('/list', categoryController.list);
-router.put('/update', categoryController.update);
-router.put('/activate', categoryController.activate);
-router.put('/deactivate', categoryController.deactivate);
-router.delete('/remove', categoryController.remove);
+// solo los administradores y almaceneros pueden usar estas rutas
+router.post('/add',auth.verifyGrocer ,categoryController.add);
+router.get('/query',auth.verifyGrocer ,categoryController.query);
+router.get('/list',auth.verifyGrocer ,categoryController.list);
+router.put('/update',auth.verifyGrocer ,categoryController.update);
+router.put('/activate',auth.verifyGrocer ,categoryController.activate);
+router.put('/deactivate',auth.verifyGrocer ,categoryController.deactivate);
+router.delete('/remove',auth.verifyGrocer ,categoryController.remove);
 
 export default router;
