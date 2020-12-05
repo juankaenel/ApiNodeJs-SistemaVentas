70fe472f3440c0617d020d03a19f8d00b24d72d9
Juan Kaenel <kaenel10@gmail.com>
2020-12-05 00:27:20 -0300

    Editando categoria

--- routes/category.js
+++ routes/category.js
@@ -4,10 +4,13 @@ import auth from '../middlewares/auth';
 const router = routerx(); // routerx usamos para que las rutas nos devuelvan un promise
 
 // solo los administradores y almaceneros pueden usar estas rutas
-router.post('/add',auth.verifyGrocer ,categoryController.add);
+//router.post('/add',auth.verifyGrocer ,categoryController.add);
+router.post('/add',categoryController.add);
 router.get('/query',auth.verifyGrocer ,categoryController.query);
-router.get('/list',auth.verifyGrocer ,categoryController.list);
-router.put('/update',auth.verifyGrocer ,categoryController.update);
+//router.get('/list',auth.verifyGrocer ,categoryController.list);
+router.get('/list',categoryController.list);
+//router.put('/update',auth.verifyGrocer ,categoryController.update);
+router.put('/update',categoryController.update);
 router.put('/activate',auth.verifyGrocer ,categoryController.activate);
 router.put('/deactivate',auth.verifyGrocer ,categoryController.deactivate);
 router.delete('/remove',auth.verifyGrocer ,categoryController.remove);
